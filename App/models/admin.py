a7ad071e032a2a686d353a59ca5b25264985ccda
Keshan Moosai <keshanmoosai@gmail,com>
2023-10-03 12:41:59 -0400

    Fixed broken import statement

--- App/models/admin.py
+++ App/models/admin.py
@@ -1,7 +1,7 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
-from user import User
+from App.models import User
 
 class Admin(User):
     def __init__(self, username, password):
