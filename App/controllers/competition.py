2d81ff6d59bb13cb65a95f5fab597620e78d783f
Keshan Moosai <keshanmoosai@gmail.com>
2023-10-05 13:02:14 -0400

    Added create competiton method

--- App/controllers/competition.py
+++ App/controllers/competition.py
@@ -1,7 +1,13 @@
+from App.database import db
 from App.models import Competition
+from datetime import date
 
-def create_competition() -> Competition:
-    return None
+def create_competition(name: str, description: str, start_date: date = None, end_date: date = None) -> Competition:
+    new_competition = Competition(name, description, start_date, end_date)
+    db.session.add(new_competition)
+    db.session.commit()
+    print("Created new competition: " + f"[{name}, {description}, {start_date}, {end_date}]")
+    return new_competition
 
 def get_competition(id) -> Competition:
     return Competition.query.get(id)
