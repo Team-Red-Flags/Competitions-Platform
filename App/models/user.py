beff7b63b520c03516ecdaa5f8701a832f838fc1
Keshan Moosai <keshanmoosai@gmail.com>
2023-10-05 12:47:17 -0400

    Code cleanup

--- App/models/user.py
+++ App/models/user.py
@@ -1,18 +1,21 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
+from sqlalchemy import Column, Integer, String
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
-    type = db.Column(db.String, nullable=False, default='user')
+    id       = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String(120), nullable=False)
+    type     = Column(String, nullable=False, default='user')
+    
 
     def __init__(self, username, password, type='user'):
         self.username = username
         self.set_password(password)
         self.type = type if type in ['user', 'admin'] else 'user'
+
 
     def get_json(self) -> dict[str]:
         return {
@@ -21,6 +24,7 @@ class User(db.Model, UserMixin):
             'type': self.type
         }
         
+        
     def is_admin(self) -> bool:
         """Checks if the user is an admin.
 
@@ -28,6 +32,7 @@ class User(db.Model, UserMixin):
             bool: True if the user is an admin. False otherwise
         """
         return self.type == 'admin'
+    
 
     def set_password(self, password: str, _method: str = 'sha256'):
         """Generates a hashed password for the user using the given method
@@ -37,6 +42,7 @@ class User(db.Model, UserMixin):
             password (str): The password to be hashed
         """
         self.password = generate_password_hash(password, method=_method)
+        
     
     def check_password(self, password: str) -> bool:
         """Checks the given password against the hashed password.
@@ -49,6 +55,7 @@ class User(db.Model, UserMixin):
         """
         return check_password_hash(self.password, password)
     
+    
     def __str__(self):
         """String representation of the user object
 
@@ -56,6 +63,7 @@ class User(db.Model, UserMixin):
             str: A prettified string representation of the user
         """
         return f"<{self.type.capitalize()} {self.id}: {self.username}>"
+    
     
     def __repr__(self):
         """Calls the __str__ method to represent the user
