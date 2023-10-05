4637542b4ea9d62387a2bcd4954b97f58e026a75
Keshan Moosai <keshanmoosai@gmail.com>
2023-10-05 12:35:54 -0400

    Minor code cleanup

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -2,11 +2,11 @@ from App.models import Admin
 from App.database import db
 
 def create_admin(username, password) -> Admin:
-    newadmin = Admin(username=username, password=password)
-    db.session.add(newadmin)
+    new_admin = Admin(username=username, password=password)
+    db.session.add(new_admin)
     db.session.commit()
     print("Created admin: " + f"[{username}, {password}]")
-    return newadmin
+    return new_admin
 
 def get_admin_by_username(username) -> Admin:
     return Admin.query.filter_by(username=username).first()
