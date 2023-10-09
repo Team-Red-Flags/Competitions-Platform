49c044d53fbaff0152a3b93b4a3a3990616e1234
Keshan Moosai <keshanmoosai@gmail.com>
2023-10-09 15:56:51 -0400

    More code fixes

--- App/views/participant.py
+++ App/views/participant.py
@@ -5,7 +5,6 @@ from flask_login import current_user, login_required
 from .index import index_views
 
 from App.controllers import (
-    get_user,
     get_competition,
     get_participant_competitions,
     create_participant
@@ -17,13 +16,11 @@ participant_views = Blueprint('participant_views', __name__, template_folder='..
 @participant_views.route('/enroll/<int:user_id>/<int:competition_id>', methods=['POST'])
 @login_required
 def enroll_participant(user_id, competition_id):
-    print(current_user)
     if not current_user.is_admin(): return jsonify(error='Not an admin'), 403
     competitions = get_participant_competitions(user_id)
-    for c in competitions:
-        if c.id == competition_id: 
-            return jsonify(error=f'{get_user(user_id).username} enrolled in this competition'), 400
-    user, competition = get_user(user_id), get_competition(competition_id)
-    if create_participant(user, competition):
-        return jsonify(message=f"Enrolled {user.username} to {competition.name}"), 200
-    return jsonify(error=f"Failed to enroll {user.username} to {competition.name}"), 400
+    for competition in competitions:
+        if competition.id == competition_id: 
+            return jsonify(error=f'User {user_id} already enrolled in this competition'), 400
+    if create_participant(user_id, competition_id):
+        return jsonify(message=f"Enrolled user {user_id} to {get_competition(competition_id).name}"), 200
+    return jsonify(error=f"Failed to enroll user {user_id} to {get_competition(competition_id).name}"), 400
