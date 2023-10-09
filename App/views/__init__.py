31042ad5faa1910e5e3834463a6c7cc753cfeac6
Keshan Moosai <keshanmoosai@gmail.com>
2023-10-08 20:23:57 -0400

    Added import for participant views

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,8 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .competition import competition_views
+from .participant import participant_views
 
 
-views = [user_views, index_views, auth_views, competition_views] 
+views = [user_views, index_views, auth_views, competition_views, participant_views] 
 # blueprints must be added to this list
