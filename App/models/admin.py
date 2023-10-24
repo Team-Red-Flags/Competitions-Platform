ad5228c93e9a52356ab79d2ac5972f4c83f2700c
Moose <keshanmoosai@gmail.com>
2023-10-24 15:31:58 -0400

    Refactored admin model

--- App/models/admin.py
+++ App/models/admin.py
@@ -3,5 +3,12 @@ from App.models import User
 
 class Admin(User):
     
-    def __init__(self, username, password):
-        super().__init__(username, password, type='admin')
+    def __init__(self, username: str, password: str):
+        """Constructor for an admin type user. Inherits from base class User
+
+        Args:
+            username (str): The username of the user
+            password (str): The plaintext password of the user. Will be hashed
+        """
+        super().__init__(username, password)
+        super().set_user_type('admin')
