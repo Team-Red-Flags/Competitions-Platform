2a24a41efdfa55e142faaff8f87be1295269727b
Moose <keshanmoosai@gmail.com>
2023-10-24 16:01:09 -0400

    Added compatibility with Student model

--- App/models/participant.py
+++ App/models/participant.py
@@ -1,28 +1,28 @@
 from App.database import db
-from App.models import User, Competition
+from App.models import Student, Competition
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 class Participant(db.Model):
     __tablename__ = "participant"
     id               = Column(Integer, primary_key=True)
-    user             = relationship('User')
+    user             = relationship('Student')
     competition      = relationship('Competition')
-    user_id          = Column(Integer, ForeignKey('user.id'))
+    student_id       = Column(Integer, ForeignKey('student.id'))
     competition_id   = Column(Integer, ForeignKey('competition.id'))
     score            = Column(Integer, default=0)
     
     
-    def __init__(self, user_id: int, competition_id: int, score: int = 0):
-        self.user_id = user_id
+    def __init__(self, student_id: int, competition_id: int, score: int = 0):
+        self.student_id = student_id
         self.competition_id = competition_id
         self.score = score
 
 
     def get_json(self):
         return {
-            'user_id': self.user_id,
-            # 'username':  User.query.get(self.user_id).username,
+            'student_id': self.student_id,
+            # 'username':  User.query.get(self.student_id).username,
             'competition_id': self.competition_id,
             # 'competition_name': Competition.query.get(self.competition_id).name,
             'score': self.score
@@ -30,7 +30,7 @@ class Participant(db.Model):
         
     
     def __str__(self):
-        return f"<Participant {self.id}: {self.user_id}, {self.competition_id}, {self.score}>"
+        return f"<Participant {self.id}: {self.student_id}, {self.competition_id}, {self.score}>"
     
     
     def __repr__(self):
