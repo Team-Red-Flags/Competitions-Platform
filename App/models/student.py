18808d61637c2d0991da7895d7ea72a6e3c75319
Moose <keshanmoosai@gmail.com>
2023-10-24 15:56:13 -0400

    Added pertinent attributes. Override get_json

--- App/models/student.py
+++ App/models/student.py
@@ -1,7 +1,17 @@
 from App.database import db
 from App.models import User
+from sqlalchemy import Column, Integer, String, Date, LargeBinary
 
 class Student(User):
+    __tablename__ = 'student'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fname = Column(String(80), nullable=False)
+    lname = Column(String(80), nullable=False)
+    student_id = Column(Integer)
+    student_email = Column(String(80), nullable=False)
+    dob =  Column(Date, nullable=False)
+    image = Column(LargeBinary, nullable=False, unique=False)
+    
     
     def __init__(self, username: str, password: str):
         """Constructor for a student type user. Inherits from base class User
@@ -12,3 +22,15 @@ class Student(User):
         """
         super().__init__(username, password)
         super().set_user_type('student')
+        
+        
+    def get_json(self) -> dict:
+        return {
+            "id": self.id,
+            "username": self.username,
+            "student_id": self.student_id,
+            "fname" : self.fname,
+            "lname" : self.lname,
+            "student_email" : self.student_email,
+            "dob" : self.dob 
+        }
