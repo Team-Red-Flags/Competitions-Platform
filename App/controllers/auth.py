719e5bd6d960c7e85198696d60ce249a5bfc112d
Moose <keshanmoosai@gmail.com>
2023-10-24 21:29:52 -0400

    Added student auth. Updated general user auth

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,23 +1,30 @@
 from flask_login import login_user, login_manager, logout_user, LoginManager
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 
-from App.models import User, Admin
+from App.models import User, Admin, Student
 
 def jwt_authenticate(username, password):
     user = User.query.filter_by(username=username).first()
     if not user or not user.check_password(password): return None 
     return create_access_token(identity=username)
 
-def authenticate_user(username, password) -> User:
-    user: User = User.query.filter_by(username=username).first()
-    if not user or not user.check_password(password): return None
-    return user
+def authenticate_student(username, password) -> Student:
+    student: Student = Student.query.filter_by(username=username).first()
+    if not student or not student.check_password(password): return None
+    return student
 
 def authenticate_admin(username, password) -> Admin:
     admin: Admin = Admin.query.filter_by(username=username).first()
     if not admin or not admin.check_password(password): return None
     if not admin.is_admin(): return None
     return admin
+
+def authenticate_user(username, password) -> User:
+    user = authenticate_admin(username, password)
+    if not user or not user.check_password(password):
+        user = authenticate_student(username, password)
+        if not user or not user.check_password(password): return None
+    return user
 
 def setup_flask_login(app):
     login_manager = LoginManager()
