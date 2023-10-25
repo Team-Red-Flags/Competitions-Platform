82ed9f051e3419e64c1f88d531435aebbbe9fc4a
Moose <keshanmoosai@gmail.com>
2023-10-24 21:03:04 -0400

    Updated admin model

--- App/models/admin.py
+++ App/models/admin.py
@@ -8,6 +8,7 @@ class Admin(User):
     user = relationship('User')
     id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     
+    
     def __init__(self, username: str, password: str):
         """Constructor for an admin type user. Inherits from base class User
 
@@ -17,3 +18,21 @@ class Admin(User):
         """
         super().__init__(username, password)
         super().set_user_type('admin')
+
+
+    def is_admin(self) -> bool:
+        """Overrides base class method to return True
+
+        Returns:
+            bool: True for all instances of Admin
+        """
+        return True
+    
+    
+    def __str__(self):
+        """String representation of the admin object
+
+        Returns:
+            str: A prettified string representation of the admin object
+        """
+        return f"<Admin {self.id}: {self.username}>"
