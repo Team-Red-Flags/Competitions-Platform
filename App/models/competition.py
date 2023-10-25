740078db9bc2ccd42ee6119c844d65a6ff490fd3
Moose <keshanmoosai@gmail.com>
2023-10-24 23:03:27 -0400

    Enforced unique attribute for competition name

--- App/models/competition.py
+++ App/models/competition.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql.functions import now
 class Competition(db.Model):
     __tablename__ = "competition"
     id          = Column(Integer, primary_key=True)
-    name        = Column(String(80), nullable=False)
+    name        = Column(String(80), nullable=False, unique=True)
     description = Column(String(200), nullable=False)
     start_date  = Column(Date, nullable=True)
     end_date    = Column(Date, nullable=True)
