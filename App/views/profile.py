c52fe39c58e587783647ee00e5f3cafda6a82196
Moose <keshanmoosai@gmail.com>
2023-10-24 21:31:01 -0400

    Moved routes in views/user.py to views/profile.py

--- App/views/profile.py
+++ App/views/profile.py
@@ -2,19 +2,16 @@ from flask import Blueprint, render_template, jsonify, request, send_from_direct
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import current_user, login_required
 
-from.index import index_views
+from .index import index_views
 
 from App.controllers import (
-    create_user,
-    jwt_authenticate, 
-    jwt_required,
     get_participant_competitions
 )
 
-user_views = Blueprint('user_views', __name__, template_folder='../templates')
+profile_views = Blueprint('profile_views', __name__, template_folder='../templates')
 
-#view profile
-@user_views.route('/profile', methods=['GET'])
+
+@profile_views.route('/profile', methods=['GET'])
 @login_required
 def view_profile():
     profile_data = {}
