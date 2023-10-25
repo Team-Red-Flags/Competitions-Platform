dcab4a464a1a144846e0a60ef598a67427effc18
Keshan Moosai <keshanmoosai@gmail,com>
2023-10-25 11:45:42 -0400

    New unit tests format

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -1,20 +1,19 @@
 import os, tempfile, pytest, logging, unittest
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from datetime import date
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User, Competition, Participant
+from App.models import User, Student, Admin, Competition, Participant
 from App.controllers import (
-    create_user,
-    get_all_users_json,
-    authenticate_user,
-    get_user,
-    get_user_by_username,
+    get_all_students_json,
+    authenticate_admin,
+    authenticate_student,
+    get_admin,
+    get_student,
     update_user,
     create_competition,
     get_competition,
     get_all_competitions
-
 )
 
 
@@ -23,62 +22,115 @@ LOGGER = logging.getLogger(__name__)
 '''
    Unit Tests
 '''
+
+# Base user unit tests
 class UserUnitTests(unittest.TestCase):
-    # pure function no side effects or integrations called
-    #User Unit Tests
+    
+    def test_new_user(self):
+        user = User("bob", "bobpass")
+        assert user.username == "bob"
+        
     def test_get_json(self):
         user = User("bob", "bobpass")
         user_json = user.get_json()
         self.assertDictEqual(user_json, {"id":None, "username":"bob"})
-    
-    def test_hashed_password(self):
-        password = "mypass"
-        hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password)
-        assert user.password != password
+
+    def test_is_admin(self):
+        user = Admin("jane", "janepass")
+        assert user.is_admin()
 
     def test_check_password(self):
         password = "mypass"
         user = User("bob", password)
         assert user.check_password(password)
+    
+    def test_hashed_password(self):
+        password = "mypass"
+        hashed = generate_password_hash(password, method='sha256')
+        user = User("bob", password)
+        assert user.password == hashed
 
-    def test_new_user(self):
-        user = User("bob", "bobpass")
-        assert user.username == "bob"
+
+# Student user unit tests
+class StudentUnitTests(unittest.TestCase):
+
+    def test_new_student(self):
+        student = Student(
+            username='rob',
+            password='robpass',
+            fname='Rob',
+            lname='Robinson',
+            student_id=80012346,
+            student_email='rob.robinson@my.uwi.edu',
+            dob = date(1998, 1, 4)
+        )
+        assert student.username == 'rob'
+    
+    def test_get_json(self):
+        student = Student(
+            username='rob',
+            password='robpass',
+            fname='Rob',
+            lname='Robinson',
+            student_id=80012346,
+            student_email='rob.robinson@my.uwi.edu',
+            dob = date(1998, 1, 4)
+        )
+        student_json = student.get_json()
+        self.assertDictEqual(student_json, {"id":None, "username":"rob"})
 
     def test_is_admin(self):
-        user = User("jane", "janepass", "admin")
-        assert user.type == "admin"
+        student = Student(
+            username='rob',
+            password='robpass',
+            fname='Rob',
+            lname='Robinson',
+            student_id=80012346,
+            student_email='rob.robinson@my.uwi.edu',
+            dob = date(1998, 1, 4)
+        )
+        assert student.is_admin() != True
 
-    #competition unit tests
-    def test_new_competition(self):
-        competition = Competition("test", "test", "2020-01-01", "2020-01-01")
-        assert competition.name == "test"
-
-    def test_competition_description(self):
-        competition = Competition("test", "2020-01-01", "2020-01-01")
-        competition.description = "A new competition!"
-        assert competition.description == "A new competition!"    
-
-    def test_get_json_competition(self):
-        competition = Competition("test", "test", "2020-01-01", "2020-01-01")
-        competition_json = competition.get_json()
-        self.assertDictEqual(competition_json, {"id":None, "name":"test", "description":"test", "start_date":"2020-01-01", "end_date":"2020-01-01"})
-
-    #participant unit tests    
-    def test_new_participant(self):
-        participant = Participant (1, 1)
-        assert participant.score == 0
-
-    def test_get_json_participant(self):
-        participant = Participant (1, 1)
-        participant_json = participant.get_json()
-        self.assertDictEqual(participant_json, { "user_id":1, "competition_id":1, "score":0})
+    def test_check_password(self):
+        password = "mypass"
+        student = Student(
+            username='rob',
+            password=password,
+            fname='Rob',
+            lname='Robinson',
+            student_id=80012346,
+            student_email='rob.robinson@my.uwi.edu',
+            dob = date(1998, 1, 4)
+        )
+        assert student.check_password(password)
     
-    #admin unit tests 
-    def test_new_admin(self):
-        user = User("jane", "janepass", "admin")
-        assert user.type == "admin"
+    def test_hashed_password(self):
+        password = "mypass"
+        hashed = generate_password_hash(password, method='sha256')
+        student = Student(
+            username='rob',
+            password=password,
+            fname='Rob',
+            lname='Robinson',
+            student_id=80012346,
+            student_email='rob.robinson@my.uwi.edu',
+            dob = date(1998, 1, 4)
+        )
+        assert student.password == hashed
+    
+# Admin user unit tests
+class AdminUnitTests(unittest.TestCase):
+    
+    def test_new_student(self): pass
+    
+    def test_get_json(self): pass
+    
+    def test_is_admin(self): pass
+    
+    def test_check_password(self): pass
+    
+    def test_hashed_password(self): pass
+    
 
 
 
@@ -110,8 +162,8 @@ class UsersIntegrationTests(unittest.TestCase):
         competition = create_competition("test", "test", "1 January, 2020", "10 January, 2020")
         assert competition.name == "test"
         
-    def test_get_all_users_json(self):
-        users_json = get_all_users_json()
+    def test_get_all_students_json(self):
+        students_json = get_all_students_json()
         self.assertListEqual([{"id":1, "username":"bob"}, {"id":2, "username":"rick"}], users_json)
 
     # Tests data changes in the database
