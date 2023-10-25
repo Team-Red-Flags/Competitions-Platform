34a6493b107117fd758dbde2603fc4472bc02381
Moose <keshanmoosai@gmail.com>
2023-10-24 22:03:52 -0400

    Updated global imports

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,8 +1,8 @@
-from .user import user_views
+from .profile import profile_views
 from .index import index_views
 from .auth import auth_views
 from .competition import competition_views
 from .participant import participant_views
 
 
-views = [user_views, index_views, auth_views, competition_views, participant_views]
+views = [profile_views, index_views, auth_views, competition_views, participant_views]
