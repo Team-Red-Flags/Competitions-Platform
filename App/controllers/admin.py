24ba9ec38383476bd573cd40fb603312280dcbef
Moose <keshanmoosai@gmail.com>
2023-10-24 22:21:36 -0400

    Updated admin controller

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -1,8 +1,14 @@
 from App.models import Admin
 from App.database import db
 
-def create_admin(username, password) -> Admin:
-    new_admin = Admin(username=username, password=password)
+def create_admin(username, password, fname, lname, image: bytes = None) -> Admin:
+    new_admin = Admin(
+        username=username, 
+        password=password,
+        fname=fname,
+        lname=lname,
+        image=image
+    )
     db.session.add(new_admin)
     db.session.commit()
     print("Created admin:", new_admin)
@@ -22,9 +28,13 @@ def get_all_admins_json() -> list:
     if not admins: return []
     return [admin.get_json() for admin in admins]
 
-def update_admin(id, username) -> Admin:
+def update_admin(id, username, password = None, fname = None, lname = None, image = None) -> Admin:
     admin = get_admin(id)
     admin.username = username
+    admin.password = password if password else admin.password
+    admin.fname = fname if fname else admin.fname
+    admin.lname = lname if lname else admin.lname
+    admin.image = image if image else admin.image
     db.session.add(admin)
     db.session.commit()
     return admin
