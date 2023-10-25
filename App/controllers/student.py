6ee4cd42a37aa686c4bc13c3bdeadb4c2ae01ce5
Moose <keshanmoosai@gmail.com>
2023-10-24 22:24:00 -0400

    Updated update_student method

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -41,9 +41,27 @@ def get_all_students_json() -> list:
     if not students: return []
     return [student.get_json() for student in students]
 
-def update_student(id, username) -> Student:
+def update_student(
+    id, 
+    username, 
+    password = None, 
+    fname = None, 
+    lname = None, 
+    student_id = None, 
+    student_email = None, 
+    dob = None,
+    image = None
+    ) -> Student:
+    
     student = get_student(id)
     student.username = username
+    student.password = password if password else student.password
+    student.fname = fname if fname else student.fname
+    student.lname = lname if lname else student.lname
+    student.student_id = student_id if student_id else student.student_id
+    student.student_email = student_email if student_email else student.student_email
+    student.dob = dob if dob else student.dob
+    student.image = image if image else student.image
     db.session.add(student)
     db.session.commit()
     return student
