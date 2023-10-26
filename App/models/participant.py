d526fa029a66c7ceae26aae53a35e4eda9bc118b
Moose <keshanmoosai@gmail.com>
2023-10-26 13:42:10 -0400

    Fixed incorrect reference to user model

--- App/models/participant.py
+++ App/models/participant.py
@@ -3,31 +3,31 @@ from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 class Participant(db.Model):
-    __tablename__ = "participant"
-    id               = Column(Integer, primary_key=True)
-    user             = relationship('Student')
-    competition      = relationship('Competition')
-    student_id       = Column(Integer, ForeignKey('student.id'))
-    competition_id   = Column(Integer, ForeignKey('competition.id'))
-    score            = Column(Integer, default=0)
+    __tablename__  = "participant"
+    user           = relationship('User')
+    competition    = relationship('Competition')
+    id             = Column(Integer, primary_key=True)
+    user_id        = Column(Integer, ForeignKey('user.id'), nullable=False)
+    competition_id = Column(Integer, ForeignKey('competition.id'), nullable=False)
+    score          = Column(Integer, default=0, nullable=True, unique=False)
     
     
-    def __init__(self, student_id: int, competition_id: int, score: int = 0):
-        self.student_id = student_id
+    def __init__(self, user_id: int, competition_id: int, score: int = 0):
+        self.user_id = user_id
         self.competition_id = competition_id
         self.score = score
 
 
     def get_json(self):
         return {
-            'student_id': self.student_id,
+            'user_id': self.user_id,
             'competition_id': self.competition_id,
             'score': self.score
         }
         
     
     def __str__(self):
-        return f"<Participant {self.id}: {self.student_id}, {self.competition_id}, {self.score}>"
+        return f"<Participant {self.id}: {self.user_id}, {self.competition_id}, {self.score}>"
     
     
     def __repr__(self):
