b0f97c4df4d8b514ae29cd4a035504b7c61c85f5
Moose <keshanmoosai@gmail.com>
2023-10-25 21:05:47 -0400

    Bug fixes for date string parsing

--- App/controllers/competition.py
+++ App/controllers/competition.py
@@ -26,8 +26,11 @@ def get_date_from_string(date_string: str) -> date:
     Returns:
         date: The converted date
     """
+    if not date_string: return None
+    
     if '-' in date_string:
-        year, month, day = date_string.split('-')
+        date_string = date_string.split('-')
+        day, month, year = date_string[0], date_string[1], date_string[2]
         if len(year) < 4: 
             raise ValueError('Unrecognised year in date string must be 4 digits. Received: ' + date_string)
         if len(month) > 2 and int(month) > 12 and int(month) < 1: 
@@ -37,7 +40,8 @@ def get_date_from_string(date_string: str) -> date:
         return date(int(year), int(month), int(day))
     
     if '/' in date_string:
-        year, month, day = date_string.split('/')
+        date_string = date_string.split('/')
+        day, month, year = date_string[0], date_string[1], date_string[2]
         if len(year) < 4: 
             raise ValueError('Unrecognised year in date string must be 4 digits. Received: ' + date_string)
         if len(month) > 2 and int(month) > 12 and int(month) < 1: 
@@ -46,7 +50,8 @@ def get_date_from_string(date_string: str) -> date:
             raise ValueError('Invalid day value for date format dd/mm/yyyy. Received: ' + date_string)
         return date(int(year), int(month), int(day))
     
-    day, month, year = date_string.split(" ")
+    date_string = date_string.split(" ")
+    day, month, year = date_string[0], date_string[1], date_string[2]
     month = month.split(",")[0].lower()
     if month in ['jan', 'january']:   month = 1
     if month in ['feb', 'february']:  month = 2
