40dda618614e43901e87057488df273c02ca9565
Keshan Moosai <keshanmoosai@gmail,com>
2023-10-27 09:24:22 -0400

    Added unenroll participant route

--- App/views/participant.py
+++ App/views/participant.py
@@ -6,7 +6,10 @@ from .index import index_views
 
 from App.controllers import (
     get_user,
+    get_admin,
+    is_participant,
     get_competition,
+    delete_participant,
     get_participant_competitions,
     create_participant
 )
@@ -19,7 +22,7 @@ participant_views = Blueprint('participant_views', __name__, template_folder='..
 def enroll_participant(user_id, competition_id):
     
     # Authenticate admin
-    if not current_user.is_admin(): return jsonify(error='Not an admin'), 403
+    if not get_admin(current_user.id): return jsonify(error='Not an admin'), 403
     
     # Verify existing user
     if not get_user(user_id):
@@ -40,3 +43,29 @@ def enroll_participant(user_id, competition_id):
         return jsonify(message=f"Enrolled user {user_id} to {get_competition(competition_id).name}"), 200
     
     return jsonify(error=f"Failed to enroll user {user_id} to {get_competition(competition_id).name}"), 400
+
+
+@participant_views.route('/unenroll/<int:user_id>/<int:competition_id>', methods=['POST'])
+@login_required
+def unenroll_participant(user_id, competition_id):
+    
+    # Authenticate admin
+    if not get_admin(current_user.id): return jsonify(error='Not an admin'), 403
+    
+    # Verify existing user
+    if not get_user(user_id):
+        return jsonify(error=f'User id {user_id} does not exist'), 400
+    
+    # Verify existing competition
+    if not get_competition(competition_id):
+        return jsonify(error=f'Competition id {competition_id} does not exist'), 400
+    
+    # Verify user is already enrolled
+    if not is_participant(user_id, competition_id):
+        return jsonify(error=f'User {user_id} not enrolled in this competition'), 400
+    
+    # Unenroll user from competition
+    if delete_participant(user_id, competition_id):
+        return jsonify(message=f"Unenrolled user {user_id} from {get_competition(competition_id).name}"), 200
+    
+    return jsonify(error=f"Failed to unenroll user {user_id} from {get_competition(competition_id).name}"), 400
